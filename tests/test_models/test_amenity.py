7a5d4b046c7430a1c01322a6753295b660755139
jose120918 <jose.esteban0918@gmail.com>
2021-02-17 13:56:54 -0500

    Update test amenity

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''test amenity '''
+
+import unittest
+import sys
+from models.amenity import Amenity
+
+
+class test_Amenity(unittest.TestCase):
+    """ Test amenity """
+
+    def test_name(self):
+        self.assertEqual(type(Amenity.name), str)
+
+    def test_instance_in_object(self):
+        """ Test instances """
+        self.assertTrue(hasattr(Amenity, "name"))
+        self.assertEqual(Amenity.name, "")
+
+if __name__ == "__main__":
+    unittest.main()

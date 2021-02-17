254429faa5327761ece7206d092532ce1feb8537
jose120918 <jose.esteban0918@gmail.com>
2021-02-17 13:58:04 -0500

    Update test base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-""" Unittest for Base Models """
-
-
+'''test models'''
 import unittest
 import re
 from models.base_model import BaseModel
@@ -13,24 +11,24 @@ class TestBaseModel(unittest.TestCase):
     '''test BaseModel'''
 
     def test_createAttr_noArgs(self):
-        ''' Create Instance w/o args'''
+        '''create Instance w/o args'''
         my_model = BaseModel()
         my_model.name = "Holberton"
         self.assertEqual(my_model.name, "Holberton")
 
     def test_id_noArgs(self):
-        ''' Checking type/value of id w/o args'''
+        '''check type/value of id w/o args'''
         my_model = BaseModel()
         self.assertTrue(my_model.id)
         self.assertEqual(type(my_model.id), str)
 
     def test_created_at_noArgs_type(self):
-        ''' Checking type of created_at w/o args'''
+        '''check type of created_at w/o args'''
         my_model = BaseModel()
         self.assertEqual(type(my_model.created_at), datetime)
 
     def test_created_at_noArgs_format(self):
-        ''' Checking format %Y-%M-%DT%H:%M:%S.%MS'''
+        '''check format %Y-%M-%DT%H:%M:%S.%MS'''
         datetime_format = re.compile(
             "\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d+$")
         my_model = BaseModel()
@@ -39,25 +37,25 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsNotNone(format_found)
 
     def test_created_at_noArgs_value(self):
-        ''' Checking value of created_at w/o args'''
+        '''check value of created_at w/o args'''
         now = datetime.now().replace(microsecond=0)
         my_model = BaseModel()
         self.assertEqual(my_model.created_at.replace(microsecond=0), now)
 
     def test_created_at_noArgs_afterSave(self):
-        ''' Checking created_at w/o args after save() '''
+        '''check created_at w/o args after save()'''
         my_model = BaseModel()
         my_created_at = my_model.created_at
         my_model.save()
         self.assertEqual(my_model.created_at, my_created_at)
 
     def test_updated_at_noArgs_type(self):
-        ''' Checking type updated_at '''
+        '''check type updated_at'''
         my_model = BaseModel()
         self.assertEqual(type(my_model.updated_at), datetime)
 
     def test_updated_at_noArgs_format(self):
-        '''Checking format %Y-%M-%DT%H:%M:%S.%MS'''
+        '''check format %Y-%M-%DT%H:%M:%S.%MS'''
         datetime_format = re.compile(
             "\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d+$")
         my_model = BaseModel()
@@ -66,29 +64,29 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsNotNone(format_found)
 
     def test_updated_at_noArgs_value(self):
-        ''' Checking value of updated_at'''
+        '''check value of updated_at'''
         now = datetime.now().replace(microsecond=0)
         my_model = BaseModel()
         self.assertEqual(my_model.updated_at.replace(microsecond=0), now)
 
     def test_updated_at_noArgs_value_afterSave(self):
-        ''' Checking value of updated_at after save()'''
+        '''check value of updated_at after save()'''
         my_model = BaseModel()
         updated_pre = my_model.updated_at
         my_model.save()
         self.assertTrue(my_model.updated_at > updated_pre)
 
     def test_str(self):
-        ''' Checking __str__ method'''
+        '''check __str__ method'''
         my_model = BaseModel()
         r = re.compile("\[BaseModel\] (.*) {.*}")
         my_str = my_model.__str__()
         self.assertIsNotNone(r.match(my_str))
 
     def test_to_dict_noAditonalAttr(self):
-        ''' Checking to_dict w/o additional Attributes'''
+        '''check to_dict w/o additional Attributes'''
         my_model = BaseModel()
-        BaseModel.name = "Holberton"
+        BaseModel.name = "holberton"
         attributes = {}
         for key, value in my_model.to_dict().items():
             if (key not in ('__class__', 'id', 'created_at', 'updated_at')):

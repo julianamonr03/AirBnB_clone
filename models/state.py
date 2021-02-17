e0e3c7b6c739565ccbd2bf4d642b86734dcb32dc
jose120918 <jose.esteban0918@gmail.com>
2021-02-17 13:46:54 -0500

    State User

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Class state that inhertis from BaseModel """
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ Public class attribute from state """
+
+    name = ""

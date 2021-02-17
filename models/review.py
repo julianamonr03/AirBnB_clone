5f753b2ae72709267b90a2bbc1a595dee8fced48
jose120918 <jose.esteban0918@gmail.com>
2021-02-17 13:47:31 -0500

    Review class

---
+++ models/review.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" State class that inhertis from BaseModel """
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """ Public class attributes from Review """
+
+    place_id = ""
+    user_id = ""
+    text = ""

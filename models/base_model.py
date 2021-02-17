7e1dbad7c285adfe50bc6d5cea84696f1601cb0b
julianamonr03 <julianamonroy03@hotmail.com>
2021-02-17 15:24:20 +0000

    UPDATE - fix file BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import models
 
 
-format_required = "%Y-%m-%dT%H:%M:%S.%f"
+format_iso = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel():
@@ -22,12 +22,10 @@ class BaseModel():
             for key, value in kwargs.items():
                 if key == "__class__":
                     continue
-                if key == "update_at" or key == "created_at":
-                    self.__dict__[key] = datetime.strptime(
-                        value, format_required)
+                if key == 'created_at' or key == 'updated_at':
+                    self.__dict__[key] = datetime.strptime(value, format_iso)
                 else:
-                    self.id = str(uuid.uuid4())
-                    self.created_at = datetime.now()
+                    self.__dict__[key] = value
         else:
             models.storage.new(self)
 

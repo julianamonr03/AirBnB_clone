58a325285d0c2e7c65a6cba4aaa04f2ce0232bfc
jose120918 <jose.esteban0918@gmail.com>
2021-02-17 13:57:40 -0500

    Update test user

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Unittest for User
+"""
+
+
+import unittest
+from models.base_model import BaseModel
+from models.user import User
+
+
+class test_User(unittest.TestCase):
+    """ Tests for attributes """
+
+    def test_name(self):
+        """ test id """
+        self.assertIsInstance(User.first_name, str)
+
+    def test_user(self):
+        self.assertTrue(hasattr(User, "email"))
+        self.assertTrue(hasattr(User, "password"))
+        self.assertTrue(hasattr(User, "first_name"))
+        self.assertTrue(hasattr(User, "last_name"))
+        self.assertEqual(User.email, "")
+        self.assertEqual(User.password, "")
+        self.assertEqual(User.first_name, "")
+        self.assertEqual(User.last_name, "")
+
+if __name__ == '__main__':
+    unittest.main()

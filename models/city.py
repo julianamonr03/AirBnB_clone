15328a9a642e8b11a02a8d17d21885a343a5530f
jose120918 <jose.esteban0918@gmail.com>
2021-02-17 13:47:17 -0500

    City class

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Class city that inhertis from BaseModel """
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ Public class attributes from city """
+
+    state_id = ""
+    name = ""

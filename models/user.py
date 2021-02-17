fc62a5482606106c92fdf5dc0ab2dd4e3efcf652
julianamonr03 <julianamonroy03@hotmail.com>
2021-02-17 19:15:31 +0000

    Change file user

--- models/user.py
+++ models/user.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Class User that inherits from BaseModel """
+""" New class User that inherits from BaseModel"""
 
 
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """ Public Class attributes  """
+    """ New Class inherits from BaseModel"""
 
     email = ""
     password = ""
